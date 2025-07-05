651c3033bb522b2a3a3b89db41bed81174e23b49
Hoya <kisesesky19@gmail.com>
2025-07-05 15:04:33 +0900

    chore: ignore eslint errors during build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
-// next.config.ts
 import type { NextConfig } from 'next';
 
 // 모듈로 만들기 위한 export 추가
 export {}; 
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config: any, { isServer }: { isServer: boolean }) => {
     if (!isServer) {
       config.resolve.fallback = {
